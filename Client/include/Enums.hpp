5f96bcb0496b627377763ab210849c383f020aa8
Miszo <kol478@gmail.com>
2019-03-15 15:43:07 +0100

    Add Message and Enums header file.

---
+++ Client/include/Enums.hpp
@@ -0,0 +1,23 @@
+//
+// Created by Artur Spek on 2019-03-15.
+//
+
+#ifndef PEACH_ENUMS_HPP
+
+#define PEACH_ENUMS_HPP
+
+namespace Peach {
+    namespace Client{
+        namespace Messages{
+
+            enum class ClientMessageType : uint32_t
+            {
+                //all client message types
+
+            };
+
+        }
+    }
+}
+
+#endif //PEACH_ENUMS_HPP
---
+++ Client/include/messages.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Artur Spek on 2019-03-15.
+//
+
+#ifndef PEACH_MESSAGES_HPP
+#define PEACH_MESSAGES_HPP
+
+namespace Peach {
+    namespace Client{
+        namespace Messages{
+
+        }
+    }
+}
+
+#endif //PEACH_MESSAGES_HPP
